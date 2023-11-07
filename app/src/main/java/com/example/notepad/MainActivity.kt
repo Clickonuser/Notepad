49fdd31f3309ed50f37dbba0f18d8f3a06a6204d
clickonuser <clickonuser@gmail.com>
2023-11-07 10:31:59 +0300

    recycler №5. Инициализировал RecyclerView в MainActivity. Настроил layoutManage. Создал arrayList и заполнил его dummy data. Настроил adapter, передав ему в конструктор arrayList, который создал

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.main_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val data = ArrayList<ToDoItem>()
+        for (i in 1..20) {
+            data.add(ToDoItem(i.toString(), i.toString()))
+        }
+
+        val adapter = CustomAdapter(data)
+        recyclerView.adapter = adapter
+
     }
 }
