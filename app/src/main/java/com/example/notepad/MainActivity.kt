14de9a18a97682a418f0a117ae6453586890f4f7
clickonuser <clickonuser@gmail.com>
2023-11-15 15:30:38 +0300

    Room... Добавил функционал по удалению элемента из списка с помощью swipe. Если случайно удалили элемент - его можно вернуть, для этого использовал Snackbar с кнопкой по отмене последнего действия

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.notepad
 
+import android.graphics.Canvas
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import com.example.notepad.room.AppDatabase
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), OnItemClick {
 
@@ -20,6 +25,8 @@ class MainActivity : AppCompatActivity(), OnItemClick {
     private lateinit var adapter: CustomAdapter
     private lateinit var db: AppDatabase
     private lateinit var todoLiveData: LiveData<List<ToDoItem>>
+
+    private lateinit var dataCopy: List<ToDoItem>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,10 +56,71 @@ class MainActivity : AppCompatActivity(), OnItemClick {
 
         todoLiveData = db.todoDao().getAllItems()
         todoLiveData.observe(this, Observer {
+            dataCopy = it
             adapter.updateList(it)
             screenDataValidation(it)
         })
 
+        // Swiping
+        val deleteIcon = ContextCompat.getDrawable(this, R.drawable.baseline_delete_24)
+        val intrinsicWidth = deleteIcon?.intrinsicWidth
+        val intrinsicHeight = deleteIcon?.intrinsicHeight
+        val background = ColorDrawable()
+        val backgroundColor = getColor(R.color.red)
+
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                // For future implementation
+                return false
+            }
+
+            override fun onChildDraw(canvas: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                                     dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+                val itemView = viewHolder.itemView
+                val itemHeight = itemView.bottom - itemView.top
+
+                // Draw the red delete background
+                background.color = backgroundColor
+                background.setBounds(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+                background.draw(canvas)
+
+                // Calculate position of delete icon
+                val iconTop = itemView.top + (itemHeight - intrinsicHeight!!) / 2
+                val iconMargin = (itemHeight - intrinsicHeight) / 2
+                val iconLeft = itemView.right - iconMargin - intrinsicWidth!!
+                val iconRight = itemView.right - iconMargin
+                val iconBottom = iconTop + intrinsicHeight
+
+                // Draw the delete icon
+                deleteIcon.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+                deleteIcon.draw(canvas)
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // this method is called when we swipe our item to left direction.
+                val position = viewHolder.adapterPosition // get element position
+                val deletedToDoItem: ToDoItem = dataCopy[position]
+
+                deleteItem(deletedToDoItem)
+                // then the Observer will run and change the list in the adapter
+
+                // below line is to display our snack-bar with action.
+                Snackbar.make(recyclerView, "Deleted " + deletedToDoItem.title, Snackbar.LENGTH_LONG)
+                    .setAction(
+                        getString(R.string.undo),
+                        View.OnClickListener {
+
+                            insertItem(deletedToDoItem)
+                            // then the Observer will run and change the list in the adapter
+
+                        }).show()
+            }
+            // at last we are adding this to our recycler view.
+        }).attachToRecyclerView(recyclerView)
     }
 
     private fun screenDataValidation(list: List<ToDoItem>) {
@@ -71,6 +139,10 @@ class MainActivity : AppCompatActivity(), OnItemClick {
 
     fun updateItem(item: ToDoItem) {
         db.todoDao().updateItem(item)
+    }
+
+    fun deleteItem(item: ToDoItem) {
+        db.todoDao().deleteItem(item)
     }
 
     override fun itemClicked(item: ToDoItem) {
